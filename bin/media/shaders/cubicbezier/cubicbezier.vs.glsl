4e2f963df06de8ac0abd6e5f6401a2c3c625f87f
Gen Afanasev <steaklive@gmail.com>
2018-09-25 17:49:28 +0200

    Remove some files...

--- bin/media/shaders/cubicbezier/cubicbezier.vs.glsl
+++
@@ -1,9 +0,0 @@
-#version 410 core
-
-in vec4 position;
-uniform mat4 mv_matrix;
-
-void main(void)
-{
-    gl_Position = mv_matrix * position;
-}
--- bin/media/shaders/springmass/update.fs.glsl
+++
@@ -1,8 +0,0 @@
-#version 410 core
-
-out vec4 color;
-
-void main(void)
-{
-    color = vec4(1.0);
-}
